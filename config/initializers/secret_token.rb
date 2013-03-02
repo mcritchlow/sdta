8124bdb781b100e03ce63d70f2cf2adec460d9b4
Matt Critchlow <mcritchlow@ucsd.edu>
2013-03-02 09:19:11 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sdta::Application.config.secret_token = '34e905504cd2a310b3a29848f53ec4687122a77558eb7d56ab9e89066631d1ded95fea67145bf30a677f06d98c2d57ff614896034250bdb13f41efacced38606'
